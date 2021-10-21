ddf8f70a15cd7fec1c9628e61a9a57ce266e2d93
dkhoa2906 <dkhoacao2906@gmail.com>
2021-10-21 18:54:43 +0700

    IT Club Casting

--- IT Club/MangChan.cpp
+++ IT Club/MangChan.cpp
@@ -1,5 +1,8 @@
     string KetQua = "True";
-
+    /*
+    n là số lượng phần tử trong mảng
+    a là mảng  
+    */
     for (int i=0; i<n; i++) {
         if (a[i]%2!=0) {
             KetQua = "False";    
