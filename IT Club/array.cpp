b550298771c311cf6cc83199f355fef8bf259a45
dkhoa2906 <dkhoacao2906@gmail.com>
2021-10-21 18:17:48 +0700

    f

---
+++ IT Club/array.cpp
@@ -0,0 +1,9 @@
+    string KetQua = "True";
+
+    for (int i=0; i<n; i++) {
+        if (a[i]%2!=0) {
+            KetQua = "False";    
+            break;
+        }   
+    }
+    cout << KetQua;
